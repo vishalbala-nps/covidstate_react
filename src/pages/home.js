072516d02f51b2d3500ff80e0a0ef58d4f5558d6
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2020-05-15 19:09:00 +0530

    Adding API Calls

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import TitleBar from '../components/titlebar.js'
-
+import axios from 'axios'
 function HomePage() {
+  React.useEffect(function() {
+    axios.get("http://127.0.0.1:8080/api/v1/messages",{mode:"no-cors"}).then(function(result) {
+      console.log(result)
+    })
+  },[])
   return (
     <>
         <TitleBar type="hometitle" />
