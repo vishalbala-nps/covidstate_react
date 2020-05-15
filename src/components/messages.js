38c201df273fb299077ac8c45f2a9a6ecb95538a
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2020-05-15 21:17:31 +0530

    Implemented Messages displaying

---
+++ src/components/messages.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Typography,Box } from '@material-ui/core';
+
+function MessagesDisplay(props) {
+    const [getmessage,setmessage] = React.useState("Loading..")
+    if (props.messagestate.loading == false && getmessage == "Loading..") {
+        if (props.messagestate.error == true) {
+            setmessage("An Error Occured. Please try again soon")
+        } else {
+            setmessage(props.messagestate.messageslist[Math.floor(Math.random()*props.messagestate.messageslist.length)])
+            let periodicID = setInterval(function() {
+                setmessage(props.messagestate.messageslist[Math.floor(Math.random()*props.messagestate.messageslist.length)])
+            },5000)
+        }
+    }
+    return (
+        <>
+            <br />
+            <Box bgcolor="info.main" color="primary.main">
+                <Typography align="center" variant="h4">{getmessage}</Typography>
+            </Box>
+        </>
+    )
+}
+
+export default MessagesDisplay;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import TitleBar from '../components/titlebar.js'
+import MessagesDisplay from '../components/messages.js'
 import axios from 'axios'
+
 function HomePage() {
+  const [getmessages,setmessages] = React.useReducer(function(state,action) {
+    if (action.type == "DATA_LOADED") {
+      return {loading:false,error:false,messageslist:action.payload}
+    } else if (action.type == "DATA_ERROR") {
+      return {loading:false,error:true,messageslist:[]}
+    } else {
+      return new Error()
+    }
+  },{loading:true,error:false,messageslist:[]})
   React.useEffect(function() {
-    axios.get("http://127.0.0.1:8080/api/v1/messages",{mode:"no-cors"}).then(function(result) {
-      console.log(result)
+    axios.get("http://covidstate.in/api/v1/messages").then(function(result) {
+      setmessages({
+        type: "DATA_LOADED",
+        payload: result.data["messages"]
+      })
+    }).catch(function() {
+      setmessages({type: "DATA_ERROR"})
     })
   },[])
   return (
     <>
         <TitleBar type="hometitle" />
+        <MessagesDisplay messagestate={getmessages}/>
     </>
   );
 }
