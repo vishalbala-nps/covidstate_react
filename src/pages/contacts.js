7965a6e2b6ef2197141c76cb72937fb0a798c842
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2020-05-19 13:42:55 +0530

    Added Contacts list

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TitleBar from '../components/titlebar.js'
-import {CircularProgress,Box,Card,CardContent,Typography,Link} from '@material-ui/core';
+import {CircularProgress,Box,Card,CardContent,Typography,Link,List,ListItem,ListItemText,Divider} from '@material-ui/core';
 import {Phone,WhatsApp,Email,Language} from '@material-ui/icons';
 import axios from 'axios'
 
@@ -27,13 +27,63 @@ function Contacts(props) {
       }) 
     })
   },[])
-
+  //Functions
+  function Createlist(props) {
+    let websitejsx;
+    let emailjsx;
+    let wajsx;
+    if (props.contactjson.contacts["website"] != null) {
+      websitejsx = (
+          <ListItem button component="a" href={props.contactjson.contacts["website"]}>
+            <ListItemText primary={<Typography style={{color:"#4B48B7"}}><Language /> {props.contactjson.contacts["website"]}</Typography>}></ListItemText>
+          </ListItem>
+      )
+    }
+    if (props.contactjson.contacts["email"] != null) {
+      emailjsx = (
+          <ListItem button component="a" href={"mailto:"+props.contactjson.contacts["email"]}>
+            <ListItemText primary={<Typography style={{color:"#4B48B7"}}><Email /> {props.contactjson.contacts["email"]}</Typography>}></ListItemText>
+          </ListItem>
+      )
+    }
+    if (props.contactjson.contacts["whatsapp"] != null) {
+      wajsx = (
+          <ListItem button component="a" href={"wa.me/"+props.contactjson.contacts["whatsapp"]}>
+            <ListItemText primary={<Typography style={{color:"#4B48B7"}}><WhatsApp /> {props.contactjson.contacts["whatsapp"]}</Typography>}></ListItemText>
+          </ListItem>
+      )
+    }
+    return (
+      <>
+        <ListItem>
+          <List>
+            <ListItem>
+              <ListItemText primary={<Typography><b>{props.contactjson.state}</b></Typography>}/>
+            </ListItem>
+            <ListItem button component="a" href={"tel:"+props.contactjson.contacts["phone"]}>
+              <ListItemText primary={<Typography style={{color:"#4B48B7"}}><Phone /> {props.contactjson.contacts["phone"]}</Typography>} />
+            </ListItem>
+            {websitejsx}
+            {emailjsx}
+            {wajsx}
+          </List>
+        </ListItem>
+        <Divider />
+      </>
+    )
+  }
   if (getcontacts.loading == true) {
     contactsjsx = (<CircularProgress />)
   } else {
     if (getcontacts.error == true) {
       contactsjsx = (<h1>Error</h1>)
     } else {
+        let contactslist = []
+        for (let key in getcontacts.messageslist) {
+          if (getcontacts.messageslist.hasOwnProperty(key)) {
+            contactslist.push({state:key,contacts:getcontacts.messageslist[key]})
+          }
+        }
         contactsjsx = (
           <Box m={1}>
             <br />
@@ -41,12 +91,17 @@ function Contacts(props) {
               <CardContent>
                 <Typography variant="h4" align="center">National Contacts</Typography>
                 <br />
-                <Typography variant="h6"><Phone /> <Link href={"tel:"+getcontacts.messageslist["India"]["phone"]}>{getcontacts.messageslist["India"]["phone"]}</Link></Typography>
-                <Typography variant="h6"><WhatsApp /> <Link href={"http://wa.me/"+getcontacts.messageslist["India"]["whatsapp"]}>{getcontacts.messageslist["India"]["whatsapp"]}</Link></Typography>
-                <Typography variant="h6"><Email /> <Link href={"mailto:"+getcontacts.messageslist["India"]["email"]}>{getcontacts.messageslist["India"]["email"]}</Link></Typography>
-                <Typography variant="h6"><Language /> <Link href={getcontacts.messageslist["India"]["website"]}>{getcontacts.messageslist["India"]["website"]}</Link></Typography>
+                <Typography variant="h6"><Phone style={{color:"#4B48B7"}}/> <Link href={"tel:"+getcontacts.messageslist["India"]["phone"]}>{getcontacts.messageslist["India"]["phone"]}</Link></Typography>
+                <Typography variant="h6"><WhatsApp style={{color:"#4B48B7"}}/> <Link href={"http://wa.me/"+getcontacts.messageslist["India"]["whatsapp"]}>{getcontacts.messageslist["India"]["whatsapp"]}</Link></Typography>
+                <Typography variant="h6"><Email style={{color:"#4B48B7"}}/> <Link href={"mailto:"+getcontacts.messageslist["India"]["email"]}>{getcontacts.messageslist["India"]["email"]}</Link></Typography>
+                <Typography variant="h6"><Language style={{color:"#4B48B7"}}/> <Link href={getcontacts.messageslist["India"]["website"]}>{getcontacts.messageslist["India"]["website"]}</Link></Typography>
               </CardContent>
             </Card>
+            <List>
+              {contactslist.map(function(item,index) {
+                return (<Createlist contactjson={item} key={index}/>)
+              })}
+            </List>
           </Box>
         )
     }
