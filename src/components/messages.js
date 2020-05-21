4d73ca535d8c564cdb735c9e78cec8bffd62e087
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2020-05-21 21:18:33 +0530

    Removed periodic changing of text

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import { Typography,Box } from '@material-ui/core';
-
-function MessagesDisplay(props) {
+import axios from 'axios'
+import apiUrl from '../components/api_url.js'
+function MessagesDisplay() {
+    const [getmessages,setmessages] = React.useReducer(function(state,action) {
+        if (action.type === "DATA_LOADED") {
+          return {loading:false,error:false,messageslist:action.payload}
+        } else if (action.type === "DATA_ERROR") {
+          return {loading:false,error:true,messageslist:[]}
+        } else {
+          return new Error()
+        }
+      },{loading:true,error:false,messageslist:[]})
     const [getmessage,setmessage] = React.useState("Loading..")
-    if (props.messagestate.loading === false && getmessage === "Loading..") {
-        if (props.messagestate.error === true) {
+    React.useEffect(function() {
+        axios.get(apiUrl+"/messages").then(function(result) {
+            setmessages({
+                type: "DATA_LOADED",
+                payload: result.data["messages"]
+            })
+        }).catch(function() {
+            setmessages({type: "DATA_ERROR"})
+        })
+    },[])
+    if (getmessages.loading === false && getmessage === "Loading..") {
+        if (getmessages.error === true) {
             setmessage("An Error Occured. Please try again soon")
         } else {
-            setmessage(props.messagestate.messageslist[Math.floor(Math.random()*props.messagestate.messageslist.length)])
-            // eslint-disable-next-line
-            let periodicID = setInterval(function() {
-                setmessage(props.messagestate.messageslist[Math.floor(Math.random()*props.messagestate.messageslist.length)])
-            },5000)
+            setmessage(getmessages.messageslist[Math.floor(Math.random()*getmessages.messageslist.length)])
         }
     }
     return (
         <>
             <br />
-            <Box bgcolor="#CEE3FE" color="#253883">
+            <Box bgcolor="#CEE3FE" color="#253883" onClick={function() {
+                if (getmessages.loading === false && getmessages.error === false) {
+                    setmessage(getmessages.messageslist[Math.floor(Math.random()*getmessages.messageslist.length)])
+                }
+            }}>
                 <Typography align="center" variant="h6"><b>{getmessage}</b></Typography>
             </Box>
         </>
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,16 +6,6 @@ import axios from 'axios'
 import apiUrl from '../components/api_url.js'
 
 function HomePage() {
-  const [getmessages,setmessages] = React.useReducer(function(state,action) {
-    if (action.type === "DATA_LOADED") {
-      return {loading:false,error:false,messageslist:action.payload}
-    } else if (action.type === "DATA_ERROR") {
-      return {loading:false,error:true,messageslist:[]}
-    } else {
-      return new Error()
-    }
-  },{loading:true,error:false,messageslist:[]})
-  
   const [getstats,setstats] = React.useReducer(function(state,action) {
     if (action.type === "DATA_LOADED") {
       return {loading:false,error:false,stats:action.payload}
@@ -26,14 +16,6 @@ function HomePage() {
     }
   },{loading:true,error:false,stats:[]})
   React.useEffect(function() {
-    axios.get(apiUrl+"/messages").then(function(result) {
-      setmessages({
-        type: "DATA_LOADED",
-        payload: result.data["messages"]
-      })
-    }).catch(function() {
-      setmessages({type: "DATA_ERROR"})
-    })
     axios.get(apiUrl+"/data?type=historical").then(function(result){
       setstats({
         type: "DATA_LOADED",
@@ -48,7 +30,7 @@ function HomePage() {
   return (
     <>
         <TitleBar type="hometitle" />
-        <MessagesDisplay messagestate={getmessages}/>
+        <MessagesDisplay/>
         <Statsdisplay statsstate={getstats} />
     </>
   );
