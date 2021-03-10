e1a5bb084d0885c335882e0814f752444b210343
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2021-03-10 21:09:06 +0530

    Added Date options (Not implemented)

--- src/components/homepage/statsdisplay.js
+++ src/components/homepage/statsdisplay.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Statstable from './table.js'
 import {Typography} from '@material-ui/core';
+import { MuiPickersUtilsProvider,KeyboardDatePicker } from '@material-ui/pickers';
 import InfectedCard from './stats_cards/infected_card.js'
 import DeathCard from './stats_cards/deaths_card.js'
 import CuredCard from './stats_cards/cured_card.js'
@@ -10,8 +11,11 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LoadingScreen from '../../components/loading_screen.js'
 import ErrorScreen from '../../components/onerror.js'
-
+import MomentUtils from '@date-io/moment';
+import moment from "moment";
 function Statsdisplay(props){
+    const [fromselectedDate,setfromselectedDate] = React.useState(moment("10/Mar/2020","DD/MMM/yyyy"))
+    const [toselectedDate,settoselectedDate] = React.useState()
     if (props.statsstate.loading === true) {
         return (<LoadingScreen />)
     } else {
@@ -21,7 +25,32 @@ function Statsdisplay(props){
             return (
             <>
                 <br />
-                <Typography data-testid="stats-date" variant="h5" align="center">As on: {props.statsstate.stats["timestamp"]["latest_updated_time"]}</Typography>
+                <Box m={1}>
+                    <MuiPickersUtilsProvider utils={MomentUtils}>
+                    <Grid container justify="center" spacing={2}>
+                        <Grid item>
+                            <KeyboardDatePicker
+                                variant="inline"
+                                disableToolbar
+                                format="DD/MMM/yyyy"
+                                margin="normal"
+                                label="From"
+                                value={fromselectedDate}
+                                onChange={setfromselectedDate}/>
+                        </Grid>
+                        <Grid item>
+                            <KeyboardDatePicker
+                                variant="inline"
+                                disableToolbar
+                                format="DD/MMM/yyyy"
+                                margin="normal"
+                                label="To"
+                                value={moment(props.statsstate.stats.timestamp.latest_updated_date,"DD/mm/yyyy").format("DD/mm/yyyy")}
+                                onChange={settoselectedDate}/>
+                        </Grid>
+                    </Grid>
+                    </MuiPickersUtilsProvider>
+                </Box>
                 <Box m={1} data-testid="stat-cards">
                     <Grid container justify="center" spacing={2}>
                         <Grid item md={4}>
@@ -38,14 +67,19 @@ function Statsdisplay(props){
                 <Box m={1} data-testid="stat-cards-more">
                     <Grid container justify="center" spacing={2}>
                         <Grid item sm={6}>
-                            <PieCard stats={props.statsstate.stats} />
-                        </Grid>
-                        <Grid item sm={6}>
                             <ActiveCard stats={props.statsstate.stats} />
                         </Grid>
                     </Grid>
                 </Box>
+                <br />
+                <Typography data-testid="stats-date" variant="h5" align="center"><b>As on: {props.statsstate.stats["timestamp"]["latest_updated_time"]}</b></Typography>
                 <Statstable statsstate={props.statsstate.stats}/>
+                <br />
+                <Grid container justify="center" spacing={2}>
+                    <Grid item md={9}>
+                        <PieCard stats={props.statsstate.stats} />
+                    </Grid>
+                </Grid>
             </>
             )
         }
